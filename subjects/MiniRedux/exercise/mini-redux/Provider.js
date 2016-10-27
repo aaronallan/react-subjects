b851bd4473e88fb20d9af68e2f1a577d61fa120b
aaronallan <aaron.a.pollock@gmail.com>
2016-10-27 14:52:04 -0400

    implementing mini redux

--- subjects/MiniRedux/exercise/mini-redux/Provider.js
+++ subjects/MiniRedux/exercise/mini-redux/Provider.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
 class Provider extends React.Component {
+  static childContextTypes = {
+    store: React.PropTypes.object
+  }
+
+  getChildContext() {
+    return {
+      store: this.props.store
+    }
+  }
+
   render() {
     return <div>{this.props.children}</div>
   }
--- subjects/MiniRedux/exercise/mini-redux/connect.js
+++ subjects/MiniRedux/exercise/mini-redux/connect.js
@@ -2,6 +2,32 @@ import React from 'react'
 
 export default function connect(mapStateToProps) {
   return function (Component) {
-    return Component
+    return class extends React.Component {
+      static contextTypes = {
+        store: React.PropTypes.object
+      }
+
+      constructor(props, context) {
+        super(props, context);
+        this.state = this.getStateFromStore();
+      }
+
+      componentDidMount() {
+        this.context.store.listen(this.listener);
+      }
+
+      listener = () => {
+        this.setState(this.getStateFromStore)
+      }
+
+      getStateFromStore() {
+        const storeState = this.context.store.getState()
+        return mapStateToProps(storeState)
+      }
+
+      render() {
+        return <Component {...this.state} dispatch={this.context.store.dispatch}/>
+      }
+    }
   }
 }
